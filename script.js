fe68d875464be9e1f0046f1a524c93b06b31619b
Andreyrvs <cortaesai@gmail.com>
2021-12-27 15:43:15 -0300

    Requisito 04 & lint + lint:styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ========== ========== //
 const getPaletteContainer = document.getElementById('color-palette');
 
 function generatePaltteColor(cores) {
@@ -11,4 +12,21 @@ generatePaltteColor('black');
 generatePaltteColor('red');
 generatePaltteColor('green');
 generatePaltteColor('blue');
- 
+
+// ========== 04 - Cria Grid de pixels ========== //
+
+const getPixelBoard = document.getElementById('pixel-board');
+
+function generatePixelBoard() {
+  for (let index = 0; index < 5; index += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.className = 'linha';
+    getPixelBoard.appendChild(pixelLine);
+    for (let linha = 0; linha < 5; linha += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      getPixelBoard.appendChild(pixel);
+    }
+  }
+}
+generatePixelBoard();
